ad11f8387dc7f21b898776ef807616d3cc0cfcde
oyemsamuel <66227827+oyemsamuel@users.noreply.github.com>
2023-07-29 05:56:50 +0100

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags - Extracts active formatting flags from the format specifier
+ * @format: The formatted string containing the format specifier
+ * @i: Pointer to the index of the current character in the format string
+ *
+ * Return: Flags indicating active formatting options.
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
+	/* Flags characters: - + 0 # ' ' */
+	/* Corresponding bit positions: 1 2 4 8  16  */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -18,12 +19,16 @@ int get_flags(const char *format, int *i)
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		{
 			if (format[curr_i] == FLAGS_CH[j])
 			{
+				/* Set the corresponding flag bit in the flags variable */
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+		}
 
+		/* Stop when no more valid flags are found */
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
@@ -32,4 +37,3 @@ int get_flags(const char *format, int *i)
 
 	return (flags);
 }
-
